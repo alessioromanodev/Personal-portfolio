9ca4278547dcd6358412846a21d9875cc450fb34
AlessioRoman <alessio.romano02dev@gmail.com>
2023-05-30 11:50:28 +0200

    header responsiveness

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,8 +5,8 @@ function Header() {
   return (
     <nav className="fixed z-50 w-full bg-altGray bg-opacity-5 dark:bg-dark dark:text-gray-100 backdrop-filter backdrop-blur-lg dark:bg-opacity-5">
       <div className="max-w-5xl mx-auto px-4">
-        <div className="flex items-center justify-between h-16">
-          <span className="text-lg text-gray-900 font-semibold">
+        <div className="flex items-center justify-center md:justify-between h-16">
+          <span className="hidden md:block text-lg text-gray-900 font-semibold">
             &#128171; Alessio Romano
           </span>
           <div className="flex space-x-4 items-center text-gray-900">
